3962e93285e2a6536373af5503f99b7f74bee027
Hussain <jakariahussain9001@gmail.com>
2021-07-13 20:53:28 +0530

    Added Volley library to call the api for registering the user when otp verification is successful.

--- app/src/main/java/com/example/crts/MainActivity.java
+++ app/src/main/java/com/example/crts/MainActivity.java
@@ -9,9 +9,22 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.ServerError;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.chaos.view.PinView;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -22,6 +35,12 @@ import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthOptions;
 import com.google.firebase.auth.PhoneAuthProvider;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
@@ -29,10 +48,10 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText phoneNumber;
     private FirebaseAuth mAuth;
-    private String verificationId;
+    private String verificationId, verification, number;
     private PinView OTP;
-    private String verification;
-    private String number;
+    private TextView alreadyVerifiedPhone;
+    Button verifyButton, sendButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,32 +66,44 @@ public class MainActivity extends AppCompatActivity {
         }
         // initializing the variables
         phoneNumber = findViewById(R.id.phoneNumberEditText);
-        Button sendButton = findViewById(R.id.sendButton);
+        sendButton = findViewById(R.id.sendButton);
         phoneNumber.setSelection(0); // cursor position
         mAuth = FirebaseAuth.getInstance();
         OTP = findViewById(R.id.otpPinView);
-        Button verifyButton = findViewById(R.id.verifyButton);
+        verifyButton = findViewById(R.id.verifyButton);
+        alreadyVerifiedPhone = findViewById(R.id.alreadyVerifiedPhone);
 
         sendButton.setOnClickListener(view -> {
             number = phoneNumber.getText().toString();
             if(isValidPhoneNUmber(number)) {
                 number = "+91" + number;
-                Toast.makeText(MainActivity.this, "OTP sent to "+number, Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Sending OTP... ", Toast.LENGTH_SHORT).show();
                 sendVerificationCode(number);
-                // going to verify otp class
-
             }else {
-                Toast.makeText(MainActivity.this, "Enter a valid phone number", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Please enter a valid number", Toast.LENGTH_SHORT).show();
             }
         });
         verifyButton.setOnClickListener(view -> {
             verification = Objects.requireNonNull(OTP.getText()).toString();
             if (verification.length()<6) {
-                Toast.makeText(MainActivity.this, "Please enter valid OTP", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Enter a valid OTP", Toast.LENGTH_SHORT).show();
             } else {
-                Toast.makeText(MainActivity.this, "Verifying OTP", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Verifying OTP...", Toast.LENGTH_SHORT).show();
 
                 verifyCode(verification);
+            }
+        });
+        // this is similar to login
+        alreadyVerifiedPhone.setOnClickListener(view -> {
+            number = phoneNumber.getText().toString();
+            if(isValidPhoneNUmber(number)) {
+                number = "+91" + number;
+                Toast.makeText(MainActivity.this, "Verifying User...", Toast.LENGTH_SHORT).show();
+                // verify if the phone number already exists in our database:
+
+            }else{
+//                    Toast.makeText(MainActivity.this, "Please enter the verified number", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Enter valid phone number to verify", Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -131,14 +162,73 @@ public class MainActivity extends AppCompatActivity {
         mAuth.signInWithCredential(credential)
                 .addOnCompleteListener(task -> {
                     if (task.isSuccessful()) {
-                        Intent myIntent = new Intent(MainActivity.this, UserHomeActivity.class);
-                        myIntent.putExtra("key", number);
-                        MainActivity.this.startActivity(myIntent);
-                        finish();
+                        // register the phone no in the database
+                        registerUser(number);
+
                     } else {
                         Toast.makeText(MainActivity.this, Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_LONG).show();
                     }
                 });
+    }
+
+    private void registerUser(String number) {
+        // send request to backend for registering the user with phone number = number:
+        // we have to send phone number by using key-value pair like: ("phone": "9800000011")
+        HashMap<String, String> parameters = new HashMap<>(); // for our app, we are using the verified phone number as parameters
+        parameters.put("phone", number);
+
+        String apiKey = "https://crtsapp.herokuapp.com/api/crts/auth/register/"; // change this whenever you upload the project to some other backend service.
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.POST,
+                apiKey,
+                new JSONObject(parameters),
+                response -> {
+                    try {
+                        if(response.getBoolean("success")){
+                            // Move on to UserHomeActivity
+                            Intent myIntent = new Intent(MainActivity.this, UserHomeActivity.class);
+                            myIntent.putExtra("key", number);
+                            MainActivity.this.startActivity(myIntent);
+                            finish();
+                        }else{
+                            Toast.makeText(MainActivity.this, "Please click on Verify again", Toast.LENGTH_SHORT).show();
+                        }
+                    } catch (JSONException jsonException) {
+                        jsonException.printStackTrace();
+                    }
+                },
+                error -> {
+
+                    NetworkResponse response = error.networkResponse;
+                    if(error instanceof ServerError && response!=null){
+                        try {
+                            String res = new String(response.data, HttpHeaderParser.parseCharset(response.headers, "utf-8"));
+                            JSONObject obj = new JSONObject(res);
+                            Toast.makeText(MainActivity.this, obj.getString("msg"), Toast.LENGTH_SHORT).show();
+
+                        }catch (JSONException | UnsupportedEncodingException jsonException){
+                            jsonException.printStackTrace();
+                        }
+                    }
+                }
+        ){
+            @Override
+            public Map<String, String> getHeaders() {
+                HashMap<String, String> headers = new HashMap<>();
+                headers.put("Content-Type", "application/json");
+                return headers;
+            }
+        };
+
+        // Adding a retry policy to ensure user can try again to login in case there is an issue with the backend.
+        int socketTime = 5000;  // 5sec time is given to register
+        RetryPolicy retryPolicy = new DefaultRetryPolicy(socketTime,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+        jsonObjectRequest.setRetryPolicy(retryPolicy);
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(jsonObjectRequest);
+
     }
 
     private void verifyCode(String code) {
