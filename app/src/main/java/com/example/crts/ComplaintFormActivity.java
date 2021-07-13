436b6be58306ec92bd2e5f335da52e5008b7e867
Hussain <jakariahussain9001@gmail.com>
2021-07-13 16:50:40 +0530

    Inteegrated the OTP functionality and the basic design of the complaint form.

--- app/src/main/java/com/example/crts/ComplaintFormActivity.java
+++ app/src/main/java/com/example/crts/ComplaintFormActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -12,6 +13,7 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,9 +21,15 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class ComplaintFormActivity extends AppCompatActivity {
 
+    // Declaring the input variables from the user in the form:
+    private String name, phone, email, address, complaint_type, complaint_detail, date;
+
+    // Declaring the editText that are to be received from user input:
+    private EditText edit_name, edit_phone, edit_email, edit_add, edit_complaint_detail, edit_date;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,31 +37,66 @@ public class ComplaintFormActivity extends AppCompatActivity {
 
         // starting the current intent with
         Intent intent = getIntent();
-        String phoneNumber = intent.getStringExtra("key"); //phone number.
+        phone = intent.getStringExtra("key"); //phone number from previous activity.
 
         // setting the phone no from the previous activity in the editText
-        EditText phone = findViewById(R.id.edit_phone);
-        phone.setText(phoneNumber);
-        phone.setFocusable(false);
+        edit_phone = findViewById(R.id.edit_phone);
+        edit_phone.setText(phone);
+        edit_phone.setFocusable(false);
+
+        // initializing the EditTexts to get the data inputs from user:
+        edit_name = findViewById(R.id.edit_name);
+        edit_email = findViewById(R.id.edit_email);
+        edit_add = findViewById(R.id.edit_add);
+        edit_complaint_detail = findViewById(R.id.edit_complaint_detail);
+        edit_date = findViewById(R.id.edit_date);
 
 
+        // final submit button:
+        Button submitButton = findViewById(R.id.submitButton);
+        submitButton.setOnClickListener(view -> {
+
+            // initializing the input variables to be stored in the database:
+            // ( phone, complaint_type, date will be initialized accordingly )
+            name = edit_name.getText().toString();
+            email = edit_email.getText().toString();
+            address = edit_add.getText().toString();
+            complaint_detail = edit_complaint_detail.getText().toString();
+            date = edit_date.getText().toString();
+
+            // check if all inputs are provided:
+            if(name.isEmpty() || email.isEmpty() || address.isEmpty() || complaint_type.isEmpty()
+                    || complaint_detail.isEmpty() || date.isEmpty()){
+                Toast.makeText(this, "All Fields are mandatory", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            // validate Email:
+            if(!isEmailValid(email)){
+                Toast.makeText(this, "Invalid Email", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            // make a post request to store the in the database:
+
+            // show a message about the success or failure of the POST request: "Complaint Registered Successfully."
+            Toast.makeText(ComplaintFormActivity.this, "Complaint Registered Successfully.", Toast.LENGTH_SHORT).show();
+        });
     }
 
 
 
     // For radio selectors
     public void onRadioClicked(View view){
-        Toast.makeText(this, "ok", Toast.LENGTH_SHORT).show();
+        // to get the text from the clicked button of the radio buttons:
+        RadioButton complaintTypeButton = findViewById(view.getId());
+        complaint_type = complaintTypeButton.getText().toString();
+
+        Toast.makeText(ComplaintFormActivity.this, complaint_type, Toast.LENGTH_SHORT).show();
     }
+
     // for date input
-    public void onCheckClicked(View view){
-
-    }
-    // for checkbox
-    EditText picked;
     public void showDateDialog(View view){
-        picked = (EditText) view;   // Store the dialog to be picked
-
+        edit_date = (EditText) view;   // Store the dialog to be picked
         DatePickerDialog datePickerDialog = new DatePickerDialog(
                 this, this::onDateSet,
                 Calendar.getInstance().get(Calendar.YEAR),
@@ -62,11 +105,23 @@ public class ComplaintFormActivity extends AppCompatActivity {
 
         datePickerDialog.show();
     }
-
+    // for setting date in UI and updating the dat evariable:
     public void onDateSet(DatePicker datePicker, int y, int m, int d) {
         // If done picking date
-        String date = d + "/" + (m+1) + "/" + y;
-        picked.setText(date);
+        String date_picked = d + "/" + (m+1) + "/" + y;
+        date = date_picked;
+        edit_date.setText(date_picked);
+    }
+    // email validator:
+    public static boolean isEmailValid(String email){
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
+                "[a-zA-Z0-9_+&*-]+)*@" +
+                "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                "A-Z]{2,7}$";
+        Pattern pat = Pattern.compile(emailRegex);
+        if (email == null)
+            return false;
+        return pat.matcher(email).matches();
     }
 
 }
--- app/src/main/java/com/example/crts/MainActivity.java
+++ app/src/main/java/com/example/crts/MainActivity.java
@@ -1,20 +1,38 @@
 package com.example.crts;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
+import com.chaos.view.PinView;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+
 public class MainActivity extends AppCompatActivity {
 
     private EditText phoneNumber;
-    private Button sendButton;
-
+    private FirebaseAuth mAuth;
+    private String verificationId;
+    private PinView OTP;
+    private String verification;
+    private String number;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,36 +40,41 @@ public class MainActivity extends AppCompatActivity {
         // removing the toolbar
         try
         {
-            this.getSupportActionBar().hide();
+            Objects.requireNonNull(this.getSupportActionBar()).hide();
         }
         catch (NullPointerException e){
             Toast.makeText(this, "Error Loading The App!!!", Toast.LENGTH_SHORT).show();
         }
         // initializing the variables
         phoneNumber = findViewById(R.id.phoneNumberEditText);
-        sendButton = findViewById(R.id.sendButton);
+        Button sendButton = findViewById(R.id.sendButton);
         phoneNumber.setSelection(0); // cursor position
+        mAuth = FirebaseAuth.getInstance();
+        OTP = findViewById(R.id.otpPinView);
+        Button verifyButton = findViewById(R.id.verifyButton);
 
-        sendButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String number = phoneNumber.getText().toString();
-                if(isValidPhoneNUmber(number)) {
-                    Toast.makeText(MainActivity.this, "OTP sent", Toast.LENGTH_SHORT).show();
+        sendButton.setOnClickListener(view -> {
+            number = phoneNumber.getText().toString();
+            if(isValidPhoneNUmber(number)) {
+                number = "+91" + number;
+                Toast.makeText(MainActivity.this, "OTP sent to "+number, Toast.LENGTH_SHORT).show();
+                sendVerificationCode(number);
+                // going to verify otp class
 
-                    // going to verify otp class
-                    Intent myIntent = new Intent(MainActivity.this, VerifyOTP.class);
-                    myIntent.putExtra("key", number);
-                    MainActivity.this.startActivity(myIntent);
-                }else {
-                    Toast.makeText(MainActivity.this, "Enter a valid phone number", Toast.LENGTH_SHORT).show();
-                }
+            }else {
+                Toast.makeText(MainActivity.this, "Enter a valid phone number", Toast.LENGTH_SHORT).show();
             }
         });
+        verifyButton.setOnClickListener(view -> {
+            verification = Objects.requireNonNull(OTP.getText()).toString();
+            if (verification.length()<6) {
+                Toast.makeText(MainActivity.this, "Please enter valid OTP", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Verifying OTP", Toast.LENGTH_SHORT).show();
 
-
-
-
+                verifyCode(verification);
+            }
+        });
     }
 
     private boolean isValidPhoneNUmber(String phoneNumber){
@@ -60,4 +83,68 @@ public class MainActivity extends AppCompatActivity {
         return(phoneNumber.matches(regex));
 
     }
+
+    private void sendVerificationCode(String number) {
+        // this method is used for getting
+        // OTP on user phone number.
+        PhoneAuthOptions options =
+                PhoneAuthOptions.newBuilder(mAuth)
+                        .setPhoneNumber(number)       // Phone number to verify
+                        .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                        .setActivity(this)                 // Activity (for callback binding)
+                        .setCallbacks(mCallBack)          // OnVerificationStateChangedCallbacks
+                        .build();
+        PhoneAuthProvider.verifyPhoneNumber(options);
+    }
+    private final PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallBack
+            = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+        // below method is used when
+        // OTP is sent from Firebase
+        @Override
+        public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+            super.onCodeSent(s, forceResendingToken);
+            verificationId = s;
+        }
+        // this method is called when user
+        // receive OTP from Firebase.
+        @Override
+        public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+            final String code = phoneAuthCredential.getSmsCode();
+            if (code != null) {
+                OTP.setText(code);
+                verifyCode(code);
+            }
+        }
+        // this method is called when firebase doesn't
+        // sends our OTP code due to any error or issue.
+        @Override
+        public void onVerificationFailed(FirebaseException e) {
+
+            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    };
+
+    private void signInWithCredential(PhoneAuthCredential credential) {
+        // inside this method we are checking if
+        // the code entered is correct or not.
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        Intent myIntent = new Intent(MainActivity.this, UserHomeActivity.class);
+                        myIntent.putExtra("key", number);
+                        MainActivity.this.startActivity(myIntent);
+                        finish();
+                    } else {
+                        Toast.makeText(MainActivity.this, Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
+    }
+
+    private void verifyCode(String code) {
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationId, code);
+        // after getting credential we are calling sign in method.
+        signInWithCredential(credential);
+    }
+
 }
